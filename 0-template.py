43022b2bc29378e0ddd0d8a76e9746b334463cdc
noodles <liuyuan@aplum.com.cn>
2020-10-23 13:23:53 +0800

    initial

---
+++ 0-template.py
@@ -0,0 +1,21 @@
+#!/usr/local/bin/python3.7
+# -*- coding: utf-8 -*-
+
+def algFunc(param):
+    pass
+
+def main():
+    param = [2, 7, 11, 15]
+    ret = algFunc(param)
+    print(ret)
+
+'''0. 模板
+
+copy from leetcode
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/reorder-list
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+'''
+if __name__ == '__main__':
+    main()
---
+++ __test.py
@@ -0,0 +1,53 @@
+#!/usr/local/bin/python3.7
+# -*- coding: utf-8 -*-
+
+
+def foreachList():
+
+    list_product = ["thinkpad", "macbook", "iphone8", "robbit"]
+
+    print('--遍历数组--')
+    for v in list_product:
+        print(v)
+
+    print('--遍历数组+索引1--')
+    for key in range(len(list_product)):
+        print(key + 1, list_product[key])
+
+    print('--遍历数组+索引2--')
+    for key, v in enumerate(list_product, 1):
+        print(key, v)
+
+    # aa = set(list_product)
+
+    # 集合交叉并补-数组/字典需要转换成集合
+    a = set([1, 2, 3, 4, 5, 100, 1000])
+    b = set([1, 2, 3, 4, 5, 128.1024])
+    # 交集
+    print(a & b)  # {1, 2, 3, 4, 5}
+    print(a.intersection(b))  # {1, 2, 3, 4, 5}
+    # 差集
+    print(a - b)  # {1000, 100}
+    print(a.difference(b))  # {1000, 100}
+
+    # 并集
+    print(a | b)  # {128.1024, 1, 2, 3, 4, 5, 100, 1000}
+    print(a.union(b))  # {128.1024, 1, 2, 3, 4, 5, 100, 1000}
+
+    # 对称差集--返回两个集合中不重复的元素
+    print(a ^ b)  # {128.1024, 100, 1000}
+    print(a.symmetric_difference(b))  # {128.1024, 100, 1000}
+
+    # for _ in range(16):
+    #     print('noodles')
+
+    x = 'ABC'
+    y = x.encode('ascii') # b'ABC'
+    print(y)
+
+
+def main():
+    foreachList()
+
+if __name__ == '__main__':
+    main()
