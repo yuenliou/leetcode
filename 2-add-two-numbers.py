db338e04686e00e2ff956c37c05803cf13706593
noodles <liuyuan@aplum.com.cn>
2020-10-23 13:30:19 +0800

    2. 两数相加

---
+++ 2-add-two-numbers.py
@@ -0,0 +1,34 @@
+#!/usr/local/bin/python3.7
+# -*- coding: utf-8 -*-
+from typing import List
+
+def twoSum(nums: List[int], target: int) -> List[int]:
+    hashtable = dict()
+    for i, num in enumerate(nums):
+        if target - num in hashtable:
+            return [hashtable[target - num], i]
+        hashtable[nums[i]] = i
+    return []
+
+def main():
+    nums = [2, 7, 11, 15]
+    target = 9
+    ret = twoSum(nums, target)
+    print(ret)
+
+
+'''2. 两数相加
+给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+
+如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+
+您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+
+示例：
+
+输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+输出：7 -> 0 -> 8
+原因：342 + 465 = 807
+'''
+if __name__ == '__main__':
+    main()
