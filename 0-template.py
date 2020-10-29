ca1f4942ffc595bd625d819fd925ef8b2c3e477f
noodles <liuyuan@aplum.com.cn>
2020-10-29 16:20:54 +0800

    template

--- 0-template.py
+++ 0-template.py
@@ -12,6 +12,9 @@ def main():
 '''0
 
 copy from leetcode
+Online Judge系统（简称OJ）是bai一个在线的判题系统。用户可以在线提交程序多种程序（如C、C++）源代码，系统对源代码进行编译和执行。
+通过（Accepted,AC）、答案错误(Wrong Answer,WA)、超时(Time Limit Exceed,TLE)、超过输出限制（Output Limit Exceed,OLE)、超内存（Memory Limit Exceed,MLE）、运行时错误（Runtime Error,RE）、格式错误（Presentation Error,PE)、无法编译（Compile Error,CE）
+
 
 '''
 if __name__ == '__main__':
