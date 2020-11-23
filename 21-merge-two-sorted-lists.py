6c265e7e6804773dd95ca204c61e03f6a8991566
noodles <liuyuan@aplum.com.cn>
2020-11-23 15:55:53 +0800

    21. 合并两个有序链表

--- 21-merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists.py
@@ -3,6 +3,18 @@
 from datatype.list_node import ListNode, MyListNode
 
 def mergeTwoLists(l1: ListNode, l2: ListNode) -> ListNode:
+    if l1 is None:
+        return l2
+    elif l2 is None:
+        return l1
+    elif l1.val < l2.val:
+        l1.next = mergeTwoLists(l1.next, l2)
+        return l1
+    else:
+        l2.next = mergeTwoLists(l1, l2.next)
+        return l2
+
+def mergeTwoLists1(l1: ListNode, l2: ListNode) -> ListNode:
     """拓展思路：递归实现"""
     dummyNode = ListNode(0)
     result = dummyNode
@@ -15,15 +27,18 @@ def mergeTwoLists(l1: ListNode, l2: ListNode) -> ListNode:
             l1 = l1.next
         result = result.next
 
-    while l1:
-        result.next = l1
-        l1 = l1.next
-        result = result.next
+    # 合并后 l1 和 l2 最多只有一个还未被合并完，我们直接将链表末尾指向未合并完的链表即可
+    result.next = l1 if l1 is not None else l2
 
-    while l2:
-        result.next = l2
-        l2 = l2.next
-        result = result.next
+    # while l1:
+    #     result.next = l1
+    #     l1 = l1.next
+    #     result = result.next
+    #
+    # while l2:
+    #     result.next = l2
+    #     l2 = l2.next
+    #     result = result.next
 
     return dummyNode.next
 
