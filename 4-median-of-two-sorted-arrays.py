fdc3e00a8b864eaaa31ec58151a07616bb6bb6a0
noodles <liuyuan@aplum.com.cn>
2020-12-08 17:15:05 +0800

    4. 寻找两个正序数组的中位数

--- 4-median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays.py
@@ -4,10 +4,9 @@ from typing import List
 
 def findMedianSortedArrays(nums1: List[int], nums2: List[int]) -> float:
     """
-    解法三：
-    https://leetcode-cn.com/problems/median-of-two-sorted-arrays/solution/xiang-xi-tong-su-de-si-lu-fen-xi-duo-jie-fa-by-w-2/
-    评论区：为了简化代码，不分情况讨论，我们使用一个小trick，我们分别找第 (m+n+1) / 2 个，和 (m+n+2) / 2 个，然后求其平均值即可，这对奇偶数均适用。加入 m+n 为奇数的话，那么其实 (m+n+1) / 2 和 (m+n+2) / 2 的值相等，相当于两个相同的数字相加再除以2，还是其本身。
-    TODO:
+    官方解法一：https://leetcode-cn.com/problems/median-of-two-sorted-arrays/solution/xun-zhao-liang-ge-you-xu-shu-zu-de-zhong-wei-s-114/
+    精选解法三：https://leetcode-cn.com/problems/median-of-two-sorted-arrays/solution/xiang-xi-tong-su-de-si-lu-fen-xi-duo-jie-fa-by-w-2/
+    评论区：为了简化代码，不分情况讨论，我们使用一个小trick，我们分别找第 (m+n+1) / 2 个，和 (m+n+2) / 2 个，然后求其平均值即可，这对奇偶数均适用。假入 m+n 为奇数的话，那么其实 (m+n+1) / 2 和 (m+n+2) / 2 的值相等，相当于两个相同的数字相加再除以2，还是其本身。
     """
     def getKth(nums1, start1, end1, nums2, start2, end2, k) -> int:
         len1 = end1 - start1 + 1
@@ -18,9 +17,27 @@ def findMedianSortedArrays(nums1: List[int], nums2: List[int]) -> float:
 
         if k == 1: return min(nums1[start1], nums2[start2])
 
+        # 数组长度小于 k/2 情况
         i = start1 + min(len1, k // 2) - 1
         j = start2 + min(len2, k // 2) - 1
 
+        """评论区：【最佳入手BEGINING】
+        //i: nums1的起始位置 j: nums2的起始位置
+        public int findKth(int[] nums1, int i, int[] nums2, int j, int k){
+            if( i >= nums1.length) return nums2[j + k - 1];//nums1为空数组
+            if( j >= nums2.length) return nums1[i + k - 1];//nums2为空数组
+            if(k == 1){
+                return Math.min(nums1[i], nums2[j]);
+            }
+            int midVal1 = (i + k / 2 - 1 < nums1.length) ? nums1[i + k / 2 - 1] : Integer.MAX_VALUE;
+            int midVal2 = (j + k / 2 - 1 < nums2.length) ? nums2[j + k / 2 - 1] : Integer.MAX_VALUE;
+            if(midVal1 < midVal2){
+                return findKth(nums1, i + k / 2, nums2, j , k - k / 2);
+            }else{
+                return findKth(nums1, i, nums2, j + k / 2 , k - k / 2);
+            }        
+        }
+        """
         if nums1[i] > nums2[j]:
             return getKth(nums1, start1, end1, nums2, j + 1, end2, k - (j - start2 + 1))
         else:
