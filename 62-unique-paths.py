81d2f4c8d1f35b9c5039474f89ef01ae062a0535
noodles <liuyuan@aplum.com.cn>
2020-12-10 17:13:58 +0800

    62. 不同路径

---
+++ 62-unique-paths.py
@@ -0,0 +1,93 @@
+#!/usr/local/bin/python3.7
+# -*- coding: utf-8 -*-
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        """状态转移方程：自顶向下
+        dp[m][n] = dp[m][n-1] + dp[m-1][n]
+        """
+        def memoize(i, j):
+            # 初始值
+            if i == 0: return 1
+            if j == 0: return 1
+            # 查备忘录
+            if cache[i][j] != 0:
+                return cache[i][j]
+            # 求解子问题
+            cache[i][j] = memoize(i, j - 1) + memoize(i - 1, j)
+            # print(i, j, cache[i][j], cache)
+            return cache[i][j]
+
+        #注意行列顺序
+        cache = [[0 for _ in range(n)] for _ in range(m)]
+        return memoize(m - 1, n - 1)
+
+    def uniquePaths1(self, m: int, n: int) -> int:
+        """状态转移方程：自底向上
+        dp[m][n] = dp[m][n-1] + dp[m-1][n]
+        """
+        dp = [[0 for _ in range(n)] for _ in range(m)]
+        for i in range(m): dp[i][0] = 1
+        for j in range(n): dp[0][j] = 1
+
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i][j-1] + dp[i-1][j]
+        # print(dp)
+        return dp[m-1][n-1]
+
+def main():
+    param = 3
+    param2 = 7
+    solution = Solution()
+    ret = solution.uniquePaths(param, param2)
+    print(ret)
+    ret = solution.uniquePaths1(param, param2)
+    print(ret)
+
+'''62. 不同路径
+
+一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为 “Start” ）。
+
+机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish” ）。
+
+问总共有多少条不同的路径？
+
+ 
+
+示例 1：
+
+
+输入：m = 3, n = 7
+输出：28
+示例 2：
+
+输入：m = 3, n = 2
+输出：3
+解释：
+从左上角开始，总共有 3 条路径可以到达右下角。
+1. 向右 -> 向右 -> 向下
+2. 向右 -> 向下 -> 向右
+3. 向下 -> 向右 -> 向右
+示例 3：
+
+输入：m = 7, n = 3
+输出：28
+示例 4：
+
+输入：m = 3, n = 3
+输出：6
+ 
+
+提示：
+
+1 <= m, n <= 100
+题目数据保证答案小于等于 2 * 109
+
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/unique-paths
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+'''
+if __name__ == '__main__':
+    main()
