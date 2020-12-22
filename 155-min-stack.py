d11e9d784a0e1339852474a9bb9bce38451affe0
noodles <liuyuan@aplum.com.cn>
2020-12-22 18:26:36 +0800

    155. 最小栈

---
+++ 155-min-stack.py
@@ -0,0 +1,93 @@
+#!/usr/local/bin/python3.7
+# -*- coding: utf-8 -*-
+import math
+
+class MinStack:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        #保存数据的栈
+        self.stack = []
+        #保存最小值的栈
+        self.helper = [math.inf]
+
+    def push(self, x: int) -> None:
+        self.stack.append(x)
+        self.helper.append(min(x, self.helper[-1]))
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.helper.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.helper[-1]
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
+def main():
+    obj = MinStack()
+    obj.push(-2)
+    obj.push(0)
+    obj.push(-3)
+
+    ret = obj.getMin()
+    print(ret)
+    obj.pop()
+
+    ret = obj.top()
+    print(ret)
+
+    ret = obj.getMin()
+    print(ret)
+
+'''155. 最小栈
+
+设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。
+
+push(x) —— 将元素 x 推入栈中。
+pop() —— 删除栈顶的元素。
+top() —— 获取栈顶元素。
+getMin() —— 检索栈中的最小元素。
+ 
+
+示例:
+
+输入：
+["MinStack","push","push","push","getMin","pop","top","getMin"]
+[[],[-2],[0],[-3],[],[],[],[]]
+
+输出：
+[null,null,null,null,-3,null,0,-2]
+
+解释：
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin();   --> 返回 -3.
+minStack.pop();
+minStack.top();      --> 返回 0.
+minStack.getMin();   --> 返回 -2.
+ 
+
+提示：
+
+pop、top 和 getMin 操作总是在 非空栈 上调用。
+
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/min-stack
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+'''
+if __name__ == '__main__':
+    main()
