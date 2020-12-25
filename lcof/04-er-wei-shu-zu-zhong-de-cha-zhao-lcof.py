d773cb8a0b8df87823a33edd03e0cc6f71b02b5f
noodles <liuyuan@aplum.com.cn>
2020-12-25 15:01:59 +0800

    剑指 Offer 04. 二维数组中的查找

---
+++ lcof/04-er-wei-shu-zu-zhong-de-cha-zhao-lcof.py
@@ -0,0 +1,88 @@
+#!/usr/local/bin/python3.7
+# -*- coding: utf-8 -*-
+from typing import List
+
+class Solution:
+    """对角线(二分)排除区域有重复：所以着手点是左下角/右上角"""
+    def findNumberIn2DArray(self, matrix: List[List[int]], target: int) -> bool:
+        """左下角开始：i-- 排除i行，j++排除j列"""
+        if len(matrix) == 0 or len(matrix[0]) == 0: return False
+        i, j = len(matrix) - 1, 0
+        while i >= 0 and j < len(matrix[0]):
+            if matrix[i][j] > target:
+                i -= 1
+            elif matrix[i][j] < target:
+                j += 1
+            else:
+                return True
+        return False
+
+    def findNumberIn2DArray1(self, matrix: List[List[int]], target: int) -> bool:
+        """右上角开始：i++ 排除i行，j--排除j列"""
+        if len(matrix) == 0 or len(matrix[0]) == 0: return False
+        i, j = 0, len(matrix[0]) - 1
+        while i < len(matrix) and j >= 0:
+            if matrix[i][j] > target:
+                j -= 1
+            elif matrix[i][j] < target:
+                i += 1
+            else:
+                return True
+        return False
+
+def main():
+    param = [
+        [1,   4,  7, 11, 15],
+        [2,   5,  8, 12, 19],
+        [3,   6,  9, 16, 22],
+        [10, 13, 14, 17, 24],
+        [18, 21, 23, 26, 30]
+    ]
+    param2 = 7
+    solution = Solution()
+    ret = solution.findNumberIn2DArray(param, param2)
+    print(ret)
+    ret = solution.findNumberIn2DArray1(param, param2)
+    print(ret)
+
+'''剑指 Offer 04. 二维数组中的查找
+
+在一个 n * m 的二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。请完成一个高效的函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+
+ 
+
+示例:
+
+现有矩阵 matrix 如下：
+
+[
+  [1,   4,  7, 11, 15],
+  [2,   5,  8, 12, 19],
+  [3,   6,  9, 16, 22],
+  [10, 13, 14, 17, 24],
+  [18, 21, 23, 26, 30]
+]
+给定 target = 5，返回 true。
+
+给定 target = 20，返回 false。
+
+ 
+
+限制：
+
+0 <= n <= 1000
+
+0 <= m <= 1000
+
+ 
+
+注意：本题与主站 240 题相同：https://leetcode-cn.com/problems/search-a-2d-matrix-ii/
+
+
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/er-wei-shu-zu-zhong-de-cha-zhao-lcof
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+'''
+if __name__ == '__main__':
+    main()
