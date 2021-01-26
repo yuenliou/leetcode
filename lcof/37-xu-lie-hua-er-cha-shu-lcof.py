8b1db7d96e4800b08bd6608564a31a80fe7bb246
noodles <liuyuan@aplum.com.cn>
2021-01-26 11:34:08 +0800

    剑指 Offer 37. 序列化二叉树

---
+++ lcof/37-xu-lie-hua-er-cha-shu-lcof.py
@@ -0,0 +1,91 @@
+#!/usr/local/bin/python3.7
+# -*- coding: utf-8 -*-
+import collections
+from typing import List
+from datatype.tree_node import TreeNode, build_binary_tree, level_order_travel, in_order_travel
+
+class Codec:
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+
+        :type root: TreeNode
+        :rtype: str
+        """
+        if not root: return "[]"
+        queue = collections.deque()
+        queue.append(root)
+        res = []
+        while queue:
+            node = queue.popleft()
+            if node:
+                res.append(str(node.val))
+                queue.append(node.left)
+                queue.append(node.right)
+            else:
+                res.append("null")
+        return '[' + ','.join(res) + ']'
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+
+        :type data: str
+        :rtype: TreeNode
+        """
+        if data == "[]": return
+        vals, i = data[1:-1].split(','), 1
+        root = TreeNode(int(vals[0]))
+        queue = collections.deque()
+        queue.append(root)
+        while queue:
+            node = queue.popleft()
+            if vals[i] != "null":
+                node.left = TreeNode(int(vals[i]))
+                queue.append(node.left)
+            i += 1
+            if vals[i] != "null":
+                node.right = TreeNode(int(vals[i]))
+                queue.append(node.right)
+            i += 1
+        return root
+
+    # Your Codec object will be instantiated and called as such:
+    # codec = Codec()
+    # codec.deserialize(codec.serialize(root))
+
+def main():
+
+    root = build_binary_tree([1,2,3,None,None,4,5])
+
+    codec = Codec()
+    ret = codec.serialize(root)
+    print('-ret-')
+    print(ret)
+
+    ret = codec.deserialize(ret)
+    print('-level-')
+    level_order_travel(ret)
+
+'''剑指 Offer 37. 序列化二叉树
+
+请实现两个函数，分别用来序列化和反序列化二叉树。
+
+示例: 
+
+你可以将以下二叉树：
+
+    1
+   / \
+  2   3
+     / \
+    4   5
+
+序列化为 "[1,2,3,null,null,4,5]"
+注意：本题与主站 297 题相同：https://leetcode-cn.com/problems/serialize-and-deserialize-binary-tree/
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/xu-lie-hua-er-cha-shu-lcof
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+'''
+if __name__ == '__main__':
+    main()
