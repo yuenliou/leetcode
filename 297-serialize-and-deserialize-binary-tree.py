c463b2213ae09f53bf6175b1b3f49ff5bacaa45a
noodles <liuyuan@aplum.com.cn>
2021-01-26 12:02:39 +0800

    297. 二叉树的序列化与反序列化

---
+++ 297-serialize-and-deserialize-binary-tree.py
@@ -0,0 +1,108 @@
+#!/usr/local/bin/python3.7
+# -*- coding: utf-8 -*-
+import collections
+from typing import List
+from datatype.tree_node import TreeNode, build_binary_tree, level_order_travel, in_order_travel
+
+class Codec:
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+
+        :type root: TreeNode
+        :rtype: str
+        """
+        if not root: return "[]"
+        queue = collections.deque()
+        queue.append(root)
+        res = []
+        while queue:
+            node = queue.popleft()
+            if node:
+                res.append(str(node.val))
+                queue.append(node.left)
+                queue.append(node.right)
+            else:
+                res.append("null")
+        return '[' + ','.join(res) + ']'
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+
+        :type data: str
+        :rtype: TreeNode
+        """
+        if data == "[]": return
+        vals, i = data[1:-1].split(','), 1
+        root = TreeNode(int(vals[0]))
+        queue = collections.deque()
+        queue.append(root)
+        while queue:
+            node = queue.popleft()
+            if vals[i] != "null":
+                node.left = TreeNode(int(vals[i]))
+                queue.append(node.left)
+            i += 1
+            if vals[i] != "null":
+                node.right = TreeNode(int(vals[i]))
+                queue.append(node.right)
+            i += 1
+        return root
+
+    # Your Codec object will be instantiated and called as such:
+    # codec = Codec()
+    # codec.deserialize(codec.serialize(root))
+
+def main():
+
+    root = build_binary_tree([1,2,3,None,None,4,5])
+
+    codec = Codec()
+    ret = codec.serialize(root)
+    print('-ret-')
+    print(ret)
+
+    ret = codec.deserialize(ret)
+    print('-level-')
+    level_order_travel(ret)
+
+'''297. 二叉树的序列化与反序列化
+
+序列化是将一个数据结构或者对象转换为连续的比特位的操作，进而可以将转换后的数据存储在一个文件或者内存中，同时也可以通过网络传输到另一个计算机环境，采取相反方式重构得到原数据。
+
+请设计一个算法来实现二叉树的序列化与反序列化。这里不限定你的序列 / 反序列化算法执行逻辑，你只需要保证一个二叉树可以被序列化为一个字符串并且将这个字符串反序列化为原始的树结构。
+
+提示: 输入输出格式与 LeetCode 目前使用的方式一致，详情请参阅 LeetCode 序列化二叉树的格式。你并非必须采取这种方式，你也可以采用其他的方法解决这个问题。
+
+ 
+
+示例 1：
+
+
+输入：root = [1,2,3,null,null,4,5]
+输出：[1,2,3,null,null,4,5]
+示例 2：
+
+输入：root = []
+输出：[]
+示例 3：
+
+输入：root = [1]
+输出：[1]
+示例 4：
+
+输入：root = [1,2]
+输出：[1,2]
+ 
+
+提示：
+
+树中结点数在范围 [0, 104] 内
+-1000 <= Node.val <= 1000
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/serialize-and-deserialize-binary-tree
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+'''
+if __name__ == '__main__':
+    main()
